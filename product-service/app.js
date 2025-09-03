330defba788a15f4dc26d46f2435e5ee3efe62ed
Kiet <kietphan2909@gmail.com>
2025-09-03 14:26:24 +1000

    Removed Redis functionality

--- product-service/app.js
+++ product-service/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { Pool } from "pg";
-import Redis from "ioredis";
 import dotenv from "dotenv";
 
 dotenv.config({ quiet: true });
@@ -9,27 +8,11 @@ const app = express();
 app.use(express.json());
 
 const pgPool = new Pool({ connectionString: process.env.PG_URL }); // RDS Postgresql Connection
-// const redisClient = Redis.createClient({ url: process.env.REDIS_URL }); // Elasticache Redis Connection
-// const redisClient = new Redis.Cluster(
-//   [{ host: process.env.REDIS_ENDPOINT, port: 6379 }],
-//   {
-//     dnsLookup: (address, callback) => callback(null, address),
-//     redisOptions: {
-//       tls: {},
-//     },
-//   }
-// );
 
 // Product Search Endpoint
 app.get("/products", async (req, res) => {
   const search = req.query.search || "";
-  const cacheKey = `products:${search}`;
 
-  // Get in cache first
-  // let products = await redisClient.get(cacheKey);
-  // if (products) return res.json(JSON.parse(products));
-
-  // Query products in DB if cache is empty
   let query = `SELECT * FROM products WHERE name ILIKE $1`;
 
   if (!search) {
@@ -37,10 +20,6 @@ app.get("/products", async (req, res) => {
   }
 
   const { rows } = await pgPool.query(query, [`%${search}%`]);
-
-  // Cache products for 1 hour
-  // if (rows.length > 0)
-  //   await redisClient.set(cacheKey, JSON.stringify(rows), { EX: 3600 });
 
   res.json(rows);
 });
